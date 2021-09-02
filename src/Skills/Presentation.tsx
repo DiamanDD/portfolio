375fc7c54f32b7325bfc1acb434be913ac3176ae
DiamanDD <Dimond@list.ru>
2021-09-02 15:14:57 +0300

    1

---
+++ src/Skills/Presentation.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import style from "./Presentation.module.css"
+
+type PresentationType={
+    title:string
+    description:string
+    urlIcon:string
+}
+
+export const Presentation=(props:PresentationType)=>{
+    return(
+        <div className={style.body}>
+
+            <div className={style.logo}><img src={`${props.urlIcon}`}/></div>
+            <div className={style.title}>{props.title}</div>
+            <div className={style.descriptios} >{props.description}</div>
+        </div>
+    )
+}
