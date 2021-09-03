81ba80a440852fe63da49908db1ec0e338d14542
DiamanDD <Dimond@list.ru>
2021-09-03 08:43:21 +0300

    121

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -7,8 +7,15 @@ export const Footer = () => {
     return (
 
         <div className={style.footer}>
-            <div className={`${commonStyle.container} ${style.headerContainer}`}>
-
+            <div className={`${commonStyle.container} ${style.footerContainer}`}>
+                <span>Дмитрий Дубровский</span>
+                <div className={style.social}>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                </div>
+                <span>&copy; 2021 Все права защищенны</span>
             </div>
         </div>
 
